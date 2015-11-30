bd85e350fab97ed460a34bda8b7dc2218760867e
Tim Delaney <tim.delaney7@mail.dcu.ie>
2015-11-30 23:17:06 +0000

    second commit

---
+++ AssignTwoTimDelaney/app/src/main/java/com/example/delaneyt/assigntwotimdelaney/LinActivity.java
@@ -0,0 +1,7 @@
+package com.example.delaneyt.assigntwotimdelaney;
+
+/**
+ * Created by delaneyt on 28/11/2015.
+ */
+public class LinActivity {
+}
---
+++ AssignTwoTimDelaney/app/src/main/java/com/example/delaneyt/assigntwotimdelaney/WebViewActivity.java
@@ -0,0 +1,7 @@
+package com.example.delaneyt.assigntwotimdelaney;
+
+/**
+ * Created by delaneyt on 25/11/2015.
+ */
+public class WebViewActivity {
+}
